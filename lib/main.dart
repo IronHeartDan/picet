093aea15de2fba9c5fd336ad4d95d3a95c233d14
Danish Khan <danishkhan.dk2.dk16@gmail.com>
2021-06-23 14:34:36 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: WebWidget(),
+    );
+  }
+}
+
+class WebWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _WebWidgetState();
+  }
+}
+
+class _WebWidgetState extends State<WebWidget> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: WebView(
+          initialUrl: 'http://192.168.0.101:5500',
+          javascriptMode: JavascriptMode.unrestricted),
+    ));
+  }
+}
